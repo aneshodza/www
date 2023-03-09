34bc5e878b0c7f5bca87f76046eef52192a840b2
Anes Hodza <anes.hodza@renuo.ch>
2023-03-09 12:53:43 +0100

    Migrate scss and basic logic

---
+++ src/pages/components/Landing.jsx
@@ -0,0 +1,80 @@
+import { useEffect, useState, useRef } from "react";
+import DownArrows from "./DownArrows";
+
+let fullName = 'anes-hodza'; let minDelay = 120;
+
+export default function Landing(props) {
+    const [name, setName] = useState('');
+    const [downArrows, setDownArrows] = useState(null);
+    const caret = useRef(null);
+    const mainBody = useRef(null);
+    const titleWrapper = useRef(null);
+
+    const appendName = (idx) => {
+        if (fullName.length === idx-1) {
+            afterName()
+            return;
+        } else {
+            setName(fullName.slice(0, idx))
+        }
+
+        if (fullName[idx] === 'd') {
+            setTimeout(() => {
+                setName(fullName.slice(0, idx) + 'z')
+                setTimeout(() => {
+                    setName(fullName.slice(0, idx) + 'zd')
+                    setTimeout(() => {
+                        setName(fullName.slice(0, idx) + 'z')
+                        setTimeout(() => {
+                            setName(fullName.slice(0, idx))
+                            setTimeout(() => {
+                                appendName(idx + 1)
+                            }, minDelay + Math.random() * 100)
+                        }, minDelay + Math.random() * 100)
+                    }, minDelay + 200 + Math.random() * 100)
+                }, minDelay + Math.random() * 100)
+            }, minDelay + Math.random() * 100)
+
+        } else {
+            setTimeout(() => {
+                appendName(idx+1)
+            }, minDelay + Math.random() * 100)
+        }
+    }
+
+    const afterName = () => {
+        titleWrapper.current.style.setProperty('--from-top',
+            titleWrapper.current.offsetTop * -1 + 'px')
+        titleWrapper.current.style.setProperty('--from-left',
+            titleWrapper.current.offsetLeft * -1 + 'px')
+        titleWrapper.current.classList.add('move-top-left')
+        setTimeout(() => {
+            titleWrapper.current.classList.add('stay')
+            caret.current.classList.add('blinking')
+            mainBody.current.classList.add('fade-in')
+            props.appRef.current.classList.add('expand')
+            setDownArrows(
+                <DownArrows href={'#about'} givenClass={"jump-in float landing-arrows"}/>)
+        }, 600)
+    }
+
+    useEffect(() => {
+        setTimeout(() => {
+            appendName(0)
+        }, minDelay*1.5)
+    }, []);
+
+    return (
+        <div className="first-section">
+            <div className="center" ref={titleWrapper}>
+                <h1>{name}<span ref={caret}>_</span></h1>
+                <div ref={mainBody} className="main-body">
+                    <h2>Just another Software Engineer</h2>
+                    <div class="about">
+                    </div>
+                </div>
+            </div>
+            { downArrows }
+        </div>
+    );
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,12 @@
+import React, { useRef } from "react";
+import Landing from "./components/Landing";
+
 export default function App() {
+  const app = useRef(null);
   return (
-    <>
-      Hello, world!
-    </>
-  )
+    <div ref={app} className="app">
+      <div id="top-of-page"></div>
+      <Landing appRef={app} />
+    </div>
+  );
 }
