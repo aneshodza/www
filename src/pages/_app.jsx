5551982c92f17fad06e6b30dc54930c82a8ce44e
anes hodza <75726773+aneshodza@users.noreply.github.com>
2023-03-09 20:24:09 +0100

    Change font to be in local dir (#6)

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,12 @@
 import '/public/styles/global.css'
+import localFont from 'next/font/local'
+
+const jbMono = localFont({ src: '../fonts/JetBrainsMono-Regular.woff2' })
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={jbMono.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
