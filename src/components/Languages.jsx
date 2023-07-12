2b810d1e2a9e0c8923d4352a8206765cfba61b25
Anes Hodza <anes.hodza@renuo.ch>
2023-07-12 16:23:44 +0200

    Begin languages

---
+++ src/components/Languages.jsx
@@ -0,0 +1,10 @@
+export default function Languages() {
+  return (
+    <div className="languages">
+      <h2>Some stuff I know</h2>
+      <div className="languages-box">
+        <h4>Frontend</h4>
+      </div>
+    </div>
+  );
+}
