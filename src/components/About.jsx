1d8f311a1dde9ba559f5e7201e4d71dbd5558d89
anes <75726773+aneshodza@users.noreply.github.com>
2023-07-12 15:36:14 +0200

    Add about page, commented out for now (#15)

---
+++ src/components/About.jsx
@@ -0,0 +1,10 @@
+import Timeline from "./Timeline";
+
+export default function About() {
+  return (
+    <div id="about">
+      <h1>About me</h1>
+      <Timeline />
+    </div>
+  )
+}
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,81 +1,93 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import DownArrows from "./DownArrows";
 
-let fullName = 'anes-hodza'; let minDelay = 120;
+let fullName = "anes-hodza";
+let minDelay = 120;
 
 export default function Landing(props) {
-    const [name, setName] = useState('');
-    const [downArrows, setDownArrows] = useState(null);
-    const caret = useRef(null);
-    const mainBody = useRef(null);
-    const titleWrapper = useRef(null);
+  const [name, setName] = useState("");
+  const [downArrows, setDownArrows] = useState(null);
+  const caret = useRef(null);
+  const mainBody = useRef(null);
+  const titleWrapper = useRef(null);
 
-    const appendName = (idx) => {
-        if (fullName.length === idx-1) {
-            afterName()
-            return;
-        } else {
-            setName(fullName.slice(0, idx))
-        }
-
-        if (fullName[idx] === 'd') {
-            setTimeout(() => {
-                setName(fullName.slice(0, idx) + 'z')
-                setTimeout(() => {
-                    setName(fullName.slice(0, idx) + 'zd')
-                    setTimeout(() => {
-                        setName(fullName.slice(0, idx) + 'z')
-                        setTimeout(() => {
-                            setName(fullName.slice(0, idx))
-                            setTimeout(() => {
-                                appendName(idx + 1)
-                            }, minDelay + Math.random() * 100)
-                        }, minDelay + Math.random() * 100)
-                    }, minDelay + 200 + Math.random() * 100)
-                }, minDelay + Math.random() * 100)
-            }, minDelay + Math.random() * 100)
-
-        } else {
-            setTimeout(() => {
-                appendName(idx+1)
-            }, minDelay + Math.random() * 100)
-        }
+  const appendName = (idx) => {
+    if (fullName.length === idx - 1) {
+      afterName();
+      props.setNameDone(true);
+      return;
+    } else {
+      setName(fullName.slice(0, idx));
     }
 
-    const afterName = () => {
-        titleWrapper.current.style.setProperty('--from-top',
-            titleWrapper.current.offsetTop * -1 + 'px')
-        titleWrapper.current.style.setProperty('--from-left',
-            titleWrapper.current.offsetLeft * -1 + 'px')
-        titleWrapper.current.classList.add('move-top-left')
+    if (fullName[idx] === "d") {
+      setTimeout(() => {
+        setName(fullName.slice(0, idx) + "z");
         setTimeout(() => {
-            titleWrapper.current.classList.add('stay')
-            caret.current.classList.add('blinking')
-            mainBody.current.classList.add('fade-in')
-            props.appRef.current.classList.add('expand')
-            setDownArrows(
-                <DownArrows href={'#about'} givenClass={"jump-in float landing-arrows"}/>)
-        }, 600)
+          setName(fullName.slice(0, idx) + "zd");
+          setTimeout(() => {
+            setName(fullName.slice(0, idx) + "z");
+            setTimeout(() => {
+              setName(fullName.slice(0, idx));
+              setTimeout(() => {
+                appendName(idx + 1);
+              }, minDelay + Math.random() * 100);
+            }, minDelay + Math.random() * 100);
+          }, minDelay + 200 + Math.random() * 100);
+        }, minDelay + Math.random() * 100);
+      }, minDelay + Math.random() * 100);
+    } else {
+      setTimeout(() => {
+        appendName(idx + 1);
+      }, minDelay + Math.random() * 100);
     }
+  };
 
-    useEffect(() => {
-        setTimeout(() => {
-            appendName(0)
-        }, minDelay*1.5)
-    }, []);
-
-    return (
-        <div className="first-section" ref={mainBody}>
-            <div className="center" ref={titleWrapper}>
-                <h1>{name}<span ref={caret}>_</span></h1>
-                <div className="main-body">
-                    <h2>Just another Software Engineer</h2>
-                    <div className="about">
-                    </div>
-                </div>
-            </div>
-            {/* <UnderConstruction /> */}
-            { downArrows }
-        </div>
+  const afterName = () => {
+    titleWrapper.current.style.setProperty(
+      "--from-top",
+      titleWrapper.current.offsetTop * -1 + "px",
     );
+    titleWrapper.current.style.setProperty(
+      "--from-left",
+      titleWrapper.current.offsetLeft * -1 + "px",
+    );
+    titleWrapper.current.classList.add("move-top-left");
+    setTimeout(() => {
+      titleWrapper.current.classList.add("stay");
+      caret.current.classList.add("blinking");
+      mainBody.current.classList.add("fade-in");
+      props.appRef.current.classList.add("expand");
+      props.appRef.current.classList.remove("contracted");
+      setDownArrows(
+        <DownArrows
+          href={"#about"}
+          givenClass={"jump-in float landing-arrows"}
+        />,
+      );
+    }, 600);
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      appendName(0);
+    }, minDelay * 1.5);
+  }, []);
+
+  return (
+    <div className="first-section" ref={mainBody}>
+      <div className="center" ref={titleWrapper}>
+        <h1 className="main-title">
+          {name}
+          <span ref={caret}>_</span>
+        </h1>
+        <div className="main-body">
+          <h2>Just another Software Engineer</h2>
+          <div className="about">
+          </div>
+        </div>
+      </div>
+      {downArrows}
+    </div>
+  );
 }
