296a04d2f1a8253a126bbab0c4dc23dabc41d942
Anes Hodza <anes.hodza04@gmail.com>
2025-05-20 23:04:20 +0200

    Begin implementing whoami chapter

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,21 @@
 import Projects from "./Projects";
 import UnderConstruction from "./UnderConstruction";
 import IconLink from "./IconLink";
+import Whoami from "./Whoami";
+import localFont from 'next/font/local'
+
+const jbMono = localFont({ src: '../fonts/JetBrainsMono-Regular.ttf' })
 
 export default function About() {
   return (
     <div id="about">
-      <h2 class="chapter-title" id="projects-header">
+      <h2 className={`chapter-title ${jbMono.className}`} id="whoami-header">
+        ~/web $&gt; whoami
+        <br />
+        anes-hodza
+      </h2>
+      <Whoami />
+      <h2 className="chapter-title" id="projects-header">
         Projects
       </h2>
       <Projects />
---
+++ src/components/Whoami.jsx
@@ -0,0 +1,5 @@
+export default function Whoami() {
+  return (
+  <p>whoami</p>
+  )
+}
