4bc375f65bec2a60f76ad2db0e98d71279365d3d
Anes Hodza <anes.hodza04@gmail.com>
2025-05-21 02:08:44 +0200

    Add images etc on whoami

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,18 +2,10 @@ import Projects from "./Projects";
 import UnderConstruction from "./UnderConstruction";
 import IconLink from "./IconLink";
 import Whoami from "./Whoami";
-import localFont from 'next/font/local'
-
-const jbMono = localFont({ src: '../fonts/JetBrainsMono-Regular.ttf' })
 
 export default function About() {
   return (
     <div id="about">
-      <h2 className={`chapter-title ${jbMono.className}`} id="whoami-header">
-        ~/web $&gt; whoami
-        <br />
-        anes-hodza
-      </h2>
       <Whoami />
       <h2 className="chapter-title" id="projects-header">
         Projects
--- src/components/Whoami.jsx
+++ src/components/Whoami.jsx
@@ -1,5 +1,38 @@
+import localFont from "next/font/local";
+import Image from "next/image";
+
+const jbMono = localFont({ src: "../fonts/JetBrainsMono-Regular.ttf" });
+const image_size = 200;
+const icon_size = 40;
+
 export default function Whoami() {
   return (
-  <p>whoami</p>
-  )
+    <div id="whoami">
+      <div id="whoami-into">
+        <div id="whoami-start">
+          <h2
+            className={`chapter-title ${jbMono.className}`}
+            id="whoami-header"
+          >
+            ~/web $&gt; whoami
+            <br />
+            anes-hodza
+          </h2>
+          <p className={`${jbMono.className}`} id="whoami-text">
+            &gt; Bsc. Computer Science
+            <br />
+            &gt; Full Stack RoR Developer
+            <br />
+            &gt; Open Source Contributor
+          </p>
+        </div>
+        <Image
+          src="/assets/me.jpg"
+          width={image_size}
+          height={image_size}
+          alt="image of me"
+        />
+      </div>
+    </div>
+  );
 }
