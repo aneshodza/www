3530c28a275345bc33bcd2518cd13500465966b1
Anes Hodza <anes.hodza04@gmail.com>
2025-05-22 08:39:03 +0200

    Add variable age counter

--- src/components/Whoami.jsx
+++ src/components/Whoami.jsx
@@ -2,12 +2,29 @@ import localFont from "next/font/local";
 import Image from "next/image";
 import technologies from "@/util/data/technologies";
 import { useRouter } from 'next/navigation';
+import { useState, useEffect } from "react";
 
 const jbMono = localFont({ src: "../fonts/JetBrainsMono-Regular.ttf" });
 const image_size = 200;
+const birthday = new Date("2004-01-14");
 
 export default function Whoami() {
   const router = useRouter();
+  const [age, setAge] = useState(0);
+
+  const incrementAge = () => {
+    const now = new Date();
+    const newDateDiff = now - birthday;
+    const newAge = newDateDiff / (1000 * 60 * 60 * 24 * 365.25);
+    setAge(newAge.toFixed(10));
+  }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      incrementAge();
+    }, 50);
+    return () => clearInterval(interval);
+  })
 
   return (
     <div id="whoami" className={jbMono.className}>
@@ -24,9 +41,13 @@ export default function Whoami() {
           <p id="whoami-text">
             &gt; Bsc. Computer Science
             <br />
+            &gt; Teaching Assistant
+            <br />
             &gt; Full Stack RoR Developer
             <br />
             &gt; Open Source Contributor
+            <br />
+            &gt; <span id="whoami-age">{age}</span> years old
           </p>
         </div>
         <Image
