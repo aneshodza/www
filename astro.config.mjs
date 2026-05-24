29eeccb5dc2912caf3b2e3ebabae5ae80fb686c8
Anes Hodza <anes.hodza04@gmail.com>
2026-05-24 16:51:42 +0200

    Fix printable CV

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,6 +24,9 @@ export default defineConfig({
   ],
   vite: {
     plugins: [tailwindcss()],
+    server: {
+      allowedHosts: ['ffa1-2a0a-d1c0-2-f300-7cd4-a016-7214-7c02.ngrok-free.app'],
+    },
   },
   trailingSlash: 'ignore',
   build: {
